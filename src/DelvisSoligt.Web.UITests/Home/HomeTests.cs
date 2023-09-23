4f1d4ade7b338bb6a7a419256fbefaf874769b01
data-miner00 <data-miner00@users.noreply.github.com>
2023-09-23 08:08:19 +0800

    test: Add dialog test

--- src/DelvisSoligt.Web.UITests/Home/HomeTests.cs
+++ src/DelvisSoligt.Web.UITests/Home/HomeTests.cs
@@ -7,17 +7,37 @@ using OpenQA.Selenium.Chrome;
 
 public sealed class HomeTests
 {
+    public const string Url = "https://localhost:7104";
+
     [Fact]
-    public void HomeTest()
+    public void NavigateToHomePage_ExpectTitleFound()
     {
-        var url = "https://localhost:7104";
         var driver = new ChromeDriver();
 
-        driver.Navigate().GoToUrl(url);
+        driver.Navigate().GoToUrl(Url);
 
         var h1 = driver.FindElement(By.TagName("h1"));
 
         h1.Text.Should().BeEquivalentTo("The Ultimate .NET Core MVC Template");
+
+        driver.Quit();
+    }
+
+    [Fact]
+    public void HomePage_ClickedOnSearchButton_ExpectPopupDialog()
+    {
+        var driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl(Url);
+
+        var searchButton = driver.FindElement(By.Id("input-button"));
+        var popupOverlay = driver.FindElement(By.Id("popupOverlay"));
+        var dialogItems = driver.FindElements(By.CssSelector("#popupOverlay ul li"));
+
+        popupOverlay.Displayed.Should().BeFalse();
+        searchButton.Click();
+        popupOverlay.Displayed.Should().BeTrue();
+        dialogItems.Count.Should().Be(6);
 
         driver.Quit();
     }
